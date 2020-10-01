343f160eff61b5fc8b86b479c91f4dcd404e702c
luisangellopez9731 <luisangellopezhernandez1997@hotmail.com>
2020-10-01 08:21:19 -0400

    start

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,42 @@
-import React, { Component, useState, useEffect } from 'react';
-import {Button} from '@material-ui/core';
+import React, {Component, useState, useEffect} from "react";
+import {
+  Button,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableCell,
+  TableRow,
+  TextField
+} from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
-import './App.css';
+import "./App.css";
 
-const getPayroll = async() => {
-  const response = await fetch('/api/payroll');
-    const body = await response.json();
+const getPayroll = async () => {
+  const response = await fetch("/api/payroll");
+  const body = await response.json();
 
-    if (response.status !== 200) throw Error(body.message);
+  if (response.status !== 200) throw Error(body.message);
 
-    return body.data;
-}
+  return body.data;
+};
 
 const App = () => {
-
+  let [adding, setAdding] = useState(false);
   let [payroll, setPayroll] = useState([]);
+
+  const add = () => {
+    setAdding(true);
+  };
 
   useState(() => {
     getPayroll().then(res => {
       console.log(res);
-    })
+      setPayroll(res);
+    });
   }, []);
 
   return (
@@ -44,12 +59,53 @@ const App = () => {
         <h1>UNAPEC nomina de profesores</h1>
       </header>
       <main>
-        <div className="payroll"></div>
-        <Button>aaa</Button>
+        <div className="payroll">
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Cedula</TableCell>
+                  <TableCell align="right">Numero de cuenta</TableCell>
+                  <TableCell align="right">Moneda</TableCell>
+                  <TableCell align="right">Monto</TableCell>
+                  <TableCell align="right"></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {payroll.map(row => (
+                  <TableRow key={row.ID}>
+                    <TableCell component="th" scope="row">
+                      {row.Document}
+                    </TableCell>
+                    <TableCell align="right">{row.AccountNumber}</TableCell>
+                    <TableCell align="right">DOP</TableCell>
+                    <TableCell align="right">{row.Salary}</TableCell>
+                    <TableCell align="right"><Button>Eliminar</Button></TableCell>
+                  </TableRow>
+                ))}
+                {adding ? (
+                  <TableRow>
+                    <TableCell><TextField id="standard-basic" label="Cedula" /></TableCell>
+                    <TableCell><TextField id="standard-basic" label="Numero de cuenta" /></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell><TextField id="standard-basic" label="Monto" /></TableCell>
+                    <TableCell><Button>Eliminar</Button></TableCell>
+                  </TableRow>
+                ) : (
+                  ""
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+        <div>
+          <Button onClick={add}>Agregar</Button>
+        </div>
+        <Button>Generar y enviar</Button>
       </main>
     </div>
   );
-}
+};
 
 // class App extends Component {
 //   state = {
--- logic.js
+++ logic.js
@@ -3,14 +3,14 @@ const mysql = require('mysql');
 const getPayroll = (req, res) => {
   const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'me',
-    password: 'secret',
+    user: 'root',
+    port: '3307',
+    password: '9teio2oa',
     database: 'ioppractica1client'
   })
 
   connection.connect();
-  connection.query('select * from nomina', (error, results, fields) => {
-    console.log(results)
+  connection.query('select ID, FirstName, LastName, AccountNumber, Salary, Document from payroll', (error, results, fields) => {
     res.send({data: results});
   })
 
